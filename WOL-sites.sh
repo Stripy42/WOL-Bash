54a38dbeafe43f923127d183168fb7b9f25f7ae2
Andrew Nathanson <andrew.nathanson@gmail.com>
2022-11-30 19:36:16 +0000

    expanded the script massivly, to have inputs. use the list or single site. Also added notes for further expansion

--- WOL-sites.sh
+++ WOL-sites.sh
@@ -8,18 +8,116 @@
 #   google.com,   1.2.3.4,  11:22:33:44:55:66
 #
 
-SiteList=SiteList.csv
 
-while IFS=, read Hostname Broadcast MAC
-do 
-  echo "Do something with ${Hostname} ${Broadcast} and ${MAC}"
-  printf "\n${Hostname}"
-  ping -c 1 -W 1 ${Hostname} > /dev/null 2>&1 && { printf "   Online" ; continue ; }
-  
-  #MAC=
-  #Broadcast=
-  PortNumber=9
-  echo -e $(echo $(printf 'f%.0s' {1..12}; printf "$(echo $MAC | sed 's/://g')%.0s" {1..16}) | sed -e 's/../\\x&/g') | socat - UDP-DATAGRAM:${Broadcast}:${PortNumber},broadcast
-  
-  
-done < ${SiteList}
+SiteListDefault=SiteList.csv
+PortNumberDefault=9
+
+###------------------------------------------------------------------------------------
+
+## Functions
+HelpEcho ()
+{
+		echo "
+		WOL tool for (Old) Linux, Written by Stripy42 aka Andrew, 2022/11/29
+		command:
+		WOL-sites.sh [hf] 
+		Dependency:
+		socat
+		SWITCHES
+		-f F <File path and name>	Site list CSV file
+		-s S <hostname>				Specify a single site in the csv site list
+		-v V 						Verbose
+		-h H						Help (this option)
+		Error levels: 0 = OK, 1 = Not OK 
+		"
+}
+
+## Choice
+while getopts "hHf:F:s:S:" c
+do
+	case $c
+	in
+	V|v) verbose="-v"; verbose2="";;
+	f|F)
+		SiteListNew="${OPTARG}"
+	;;
+	s|S)
+		Site="${OPTARG}"
+	;;
+	p|P)
+		PortNumberNew="${OPTARG}"
+	;;
+	h|H) #Help
+		HelpEcho
+		exit 1
+	;;
+	?|*) #Help
+		echo "
+		Invalid option: -${OPTARG}
+		" >&2
+		HelpEcho
+		exit 1
+	;;
+	'')
+		HelpEcho
+		exit 1
+	;;
+	esac
+done
+shift $((OPTIND - 1))
+
+narg=${#@}
+n=0
+
+testids="${@}"
+
+###------------------------------------------------------------------------------------
+
+SiteList="${SiteListNew:-${SiteListDefault}}"
+PortNumber="${PortNumberNew:-${PortNumberDefault}}"
+
+CheckandWOL() {
+		echo -e $(echo $(printf 'f%.0s' {1..12}; printf "$(echo $MAC | sed 's/://g')%.0s" {1..16}) | sed -e 's/../\\x&/g') | socat - UDP-DATAGRAM:${Broadcast}:${PortNumber},broadcast
+}
+
+LoopWholeSiteList() {
+	while IFS=, read Hostname Broadcast MAC
+	do 
+		echo "Do something with ${Hostname} ${Broadcast} and ${MAC}"
+		printf "\n${Hostname}"
+		ping -c 1 -W 1 ${Hostname} > /dev/null 2>&1 && { printf "   Online" ; continue ; }
+		CheckandWOL
+	done < ${SiteList}
+}
+
+WakeOneSite() {	
+	IFS=,
+	read Hostname Broadcast MAC <<<"$(grep "${Site}" "${SiteList}" 2>/dev/null)"
+	CheckandWOL
+}
+
+
+if [[ -n ${Site}]]; then
+	LoopWholeSiteList
+else
+	WakeOneSite
+fi
+
+
+exit 0
+
+####################################
+
+#Notes on other requirements.
+
+# All require root
+if [[ $EUID -ne 0 ]]; then echo -e "This script must be run as root\n"; exit 1; fi
+
+
+# Identify if WOL enabled
+
+
+
+# Enable WOL
+
+# Create list of required IP addresses and MAC from hostnames
